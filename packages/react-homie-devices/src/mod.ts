82981a036775d307dca0a46d39932345eaa80d05
Niklas Stadigs <niklas.stadigs@gmail.com>
2024-11-25 09:19:14 +0100

    try persistence mode

--- packages/react-homie-devices/src/mod.ts
+++ packages/react-homie-devices/src/mod.ts
@@ -40,100 +40,17 @@ const reconciler = ReactReconciler<
   unknown,
   unknown,
   unknown,
-  unknown,
+  unknown[], // ChildSet
   unknown,
   unknown
 >({
-  supportsMutation: true,
-  isPrimaryRenderer: true,
-  supportsPersistence: false,
+  supportsMutation: false,
+  supportsPersistence: true,
   supportsHydration: false,
-  noTimeout: -1,
 
-  afterActiveInstanceBlur() {
-    console.log("afterActiveInstanceBlur called");
-  },
-  beforeActiveInstanceBlur() {
-    console.log("beforeActiveInstanceBlur called");
-  },
-  preparePortalMount() {
-    console.log("preparePortalMount called");
-  },
-
-  prepareScopeUpdate() {
-    console.log("prepareScopeUpdate called");
-  },
-
-  resetAfterCommit(container) {
-    console.log("resetAfterCommit called");
-    container.changes = [];
-  },
-
-  commitHydratedContainer() {
-    console.log("commitHydratedContainer called");
-  },
-
-  cancelTimeout(id: number) {
-    clearTimeout(id);
-  },
-
-  detachDeletedInstance() {
-    console.log("detachDeletedInstance called");
-  },
-
-  scheduleTimeout(cb, ms) {
-    return setTimeout(cb, ms);
-  },
-
-  finalizeInitialChildren(instance) {
-    console.log("finalizeInitialChildren called with", instance);
-    return true;
-  },
-
-  getChildHostContext(parentHostContext, type) {
-    console.log("getChildHostContext called with", parentHostContext, type);
-    // I think we should pass along the nearest parent device here
-    return parentHostContext;
-  },
-
-  getPublicInstance(instance) {
-    return instance;
-  },
-
-  getRootHostContext(rootContet) {
-    console.log("getRootHostContext called", rootContet);
-    // I think we should pass along the nearest parent device here
-    return rootContet;
-  },
-  prepareForCommit() {
-    console.log("prepareForCommit called");
-    return {};
-  },
-
-  getCurrentEventPriority() {
-    console.log("getCurrentEventPriority called");
-    return 0;
-  },
-
-  getInstanceFromNode(node: any) {
-    console.log("getInstanceFromNode called with", node);
-    return null;
-  },
-
-  getInstanceFromScope() {
-    console.log("getInstanceFromScope called");
-    return null;
-  },
-
-  prepareUpdate() {
-    console.log("prepareUpdate called");
-    return {};
-  },
-
-  shouldSetTextContent() {
-    console.log("shouldSetTextContent called");
-    return false;
-  },
+  // -------------------
+  //    Core Methods
+  // -------------------
 
   createInstance(type, props) {
     if (type === "device$") {
@@ -157,58 +74,136 @@ const reconciler = ReactReconciler<
     );
   },
 
-  commitUpdate() {
-    console.log("commitUpdate called");
-  },
-
-  removeChildFromContainer() {
-    console.log("removeChildFromContainer called");
-  },
-
-  removeChild(parentInstance, child) {
-    console.log("removeChild called");
-  },
-
-  clearContainer() {
-  },
-
   createTextInstance() {
-    console.log("createTextInstance called");
+    throw new Error("Text nodes are not supported");
   },
 
-  appendChildToContainer(
-    container,
-    child: Instance,
-  ) {
-    if (child.type !== "device") {
-      throw new Error("Only Device components can be added to the root");
-    }
-
-    container.devices[child.props.id] = child;
-  },
-
-  appendChild(parentInstance, child: Instance) {
-    if (parentInstance.type === "device") {
-      if (child.type !== "node") {
-        throw new Error("Only Node components can be added to a Device");
-      }
-
-      parentInstance.nodes[child.props.id] = child;
-    } else if (parentInstance.type === "node") {
-      if (child.type !== "property") {
-        throw new Error("Only Property components can be added to a Node");
-      }
-
-      parentInstance.properties[child.props.id] = child;
-    }
-  },
   appendInitialChild() {
     console.log("appendInitialChild called");
   },
 
-  commitMount(instance) {
-    // Apply all changes to mqtt here, I guess
-    console.log("commitMount called with");
+  finalizeInitialChildren(instance) {
+    console.log("finalizeInitialChildren called with", instance);
+    return true;
+  },
+
+  prepareUpdate() {
+    console.log("prepareUpdate called");
+    return {};
+  },
+
+  shouldSetTextContent() {
+    console.log("shouldSetTextContent called");
+    return false;
+  },
+
+  getRootHostContext(rootContet) {
+    console.log("getRootHostContext called", rootContet);
+    return rootContet;
+  },
+
+  getChildHostContext(parentHostContext, type) {
+    console.log("getChildHostContext called with", parentHostContext, type);
+    return parentHostContext;
+  },
+
+  getPublicInstance(instance) {
+    return instance;
+  },
+
+  prepareForCommit() {
+    console.log("prepareForCommit called");
+    return {};
+  },
+
+  resetAfterCommit(container) {
+    console.log("resetAfterCommit called");
+    container.changes = [];
+  },
+
+  preparePortalMount() {
+    console.log("preparePortalMount called");
+  },
+
+  scheduleTimeout(cb, ms) {
+    return setTimeout(cb, ms);
+  },
+
+  cancelTimeout(id: number) {
+    clearTimeout(id);
+  },
+
+  noTimeout: -1,
+
+  isPrimaryRenderer: true,
+
+  getCurrentEventPriority() {
+    console.log("getCurrentEventPriority called");
+    return 0;
+  },
+
+  getInstanceFromNode(node: any) {
+    console.log("getInstanceFromNode called with", node);
+    return null;
+  },
+
+  beforeActiveInstanceBlur() {
+    console.log("beforeActiveInstanceBlur called");
+  },
+
+  afterActiveInstanceBlur() {
+    console.log("afterActiveInstanceBlur called");
+  },
+
+  prepareScopeUpdate() {
+    console.log("prepareScopeUpdate called");
+  },
+
+  getInstanceFromScope() {
+    console.log("getInstanceFromScope called");
+    return null;
+  },
+
+  detachDeletedInstance() {
+    console.log("detachDeletedInstance called");
+  },
+
+  // -------------------
+  // Persistence Methods
+  // -------------------
+
+  cloneInstance(
+    instance,
+    updatePayload,
+    type,
+    oldProps,
+    newProps,
+    internalInstanceHandle,
+    keepChildren,
+    recyclableInstance,
+  ) {
+    console.log("cloneInstance called");
+    return { ...instance, props: newProps };
+  },
+
+  createContainerChildSet(container) {
+    console.log("createContainerChildSet called");
+    return [];
+  },
+
+  appendChildToContainerChildSet(childSet, child) {
+    console.log("appendChildToContainerChildSet called");
+    childSet.push(child);
+  },
+
+  finalizeContainerChildren(container, newChildren) {
+    console.log("finalizeContainerChildren called");
+    container.devices = newChildren;
+  },
+
+  replaceContainerChildren(container, newChildren) {
+    console.log("replaceContainerChildren called");
+    container.devices = newChildren;
   },
 });
 
